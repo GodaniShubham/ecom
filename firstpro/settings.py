5271673098dc7bf37dd12702c1d91ff9abdbcc43
GodaniShubham <godanishubham30@gmail.com>
2025-08-25 09:48:33 +0530

    Resolved merge conflicts

--- firstpro/settings.py
+++ firstpro/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'jazzmin',
-    "material.admin",
+    # "material.admin",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
